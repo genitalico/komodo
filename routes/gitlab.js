4f419346bbe3bd12389bab970e3845e391fcf4cf
Galileo Teco <g@80bits.com>
2020-05-01 16:38:05 -0500

    added fn for gitlab push and pipeline
    copy from other project

---
+++ routes/gitlab.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let router = express.Router();
+let whTransaction = require('../transactions/gitlab/webHooksTransaction');
+
+router.post('/webhooks/:chatId', (req, res, next) => {
+
+    let bodyReq = req.body;
+    let kindof = req.header('X-Gitlab-Event');
+    let body = "";
+    
+
+    if (kindof == "Push Hook") {
+        
+        body = whTransaction.PushEvent(bodyReq);
+        let chatId = req.params.chatId;
+        let header = 'GitLab: ' + req.body.object_kind;
+
+        let modelBot = {
+            chatId,
+            header,
+            body
+        };
+    }
+
+    res.contentType('application/json');
+    res.status(200);
+    res.json();
+    res.end();
+
+});
+
+module.exports = router;
+
---
+++ transactions/gitlab/webHooksTransaction.js
@@ -0,0 +1,40 @@
+exports.PushEvent = (model) => {
+
+    let object_kind = ['#GitLab ', model.object_kind, '\n'].join('');
+    let author = ['*Author:* ', model.user_name, '\n'].join('');
+    let project = ['*Project:* ', model.project.name, '\n'].join('');
+    let totalCommits = ['*Total Commits:* ', model.total_commits_count, '\n'].join('');
+    let commitsMsg = [];
+    for (let i = 0; i < model.commits.length; i++) {
+        commitsMsg.push((i + 1) + ": " + model.commits[i].message + '\n');
+    }
+    let commitsMsgText = commitsMsg.join('');
+    let commitsMsgText2 = ['*Commits:* \n', commitsMsgText].join('');
+    let branch = ['*Branch:* ', model.ref.replace('refs/heads/', ''), '\n'].join('');
+    let urlProject = ['\n', '[Url Project]', '(', model.repository.homepage, ')', '\n\n'].join('');
+    let compare = ['[Compare Changes]', '(', model.repository.homepage, , '/compare/', model.before, '...', model.after, ')\n\n'].join('');
+    let text = [author, project, branch, totalCommits, urlProject, compare, commitsMsgText2].join('');
+
+    return text;
+}
+
+exports.Pipeline = (model) => {
+
+    let status = ['*Status:* ', model.object_attributes.status, '\n'].join('');
+    let stages = model.object_attributes.stages.join(',');
+    let stagesText = ['*Stages:* ', stages, '\n'].join('');
+    let duration = ['*Duration:* ', model.object_attributes.duration, '\n'].join('');
+    let project = ['*Project:* ', model.project.name, '\n'].join('');
+    let builds = [];
+
+    for (let i = 0; i < model.builds.length; i++) {
+        let names = [i + 1, ': ', model.builds[i].name, '\n'].join('');
+        builds.push(names);
+    }
+
+    let buildText = ['*Builds:* ', '\n', builds.join(''), '\n'].join('');
+
+    let text = [project, status, stagesText, duration, buildText].join('');
+
+    return text;
+}
