00dbec3dbe71036172aa0e187ca53defb7dbc0c7
Galileo Teco <g@80bits.com>
2020-05-08 19:18:57 -0500

    added mongo transaction for search an user and authenticate him

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,28 +1,36 @@
 const settings = require('../appsettings');
+const findByQuery = require('../mongodb/transactions').FindByQuery;
 
 exports.authorization = function (app, opts) {
 
-    return function Invoke(req, res, next) {
+    return async function Invoke(req, res, next) {
 
         if (req.path == "/") {
             next();
             return;
         }
 
-        let authorization = 'abc';
+        let hauthorization = req.headers['authorization'] || req.headers['x-gitlab-token'];
 
-        //for others methods
-        if (req.headers['authorization'] == authorization) {
-            next();
-            return;
-        }
-        
-        //for gitlab webhooks
-        if (req.headers['X-Gitlab-Token'] == authorization) {
-            next();
+        if (hauthorization == undefined) {
+            res.contentType('application/json');
+            res.status(401);
+            res.json();
+            res.end();
             return;
         }
 
+        let query = {
+            "type": 1,
+            "token": hauthorization
+        };
+
+        let authorization = await findByQuery(req.db, query);
+
+        if (authorization.length == 1) {
+            next();
+            return;
+        }
 
         res.contentType('application/json');
         res.status(401);
